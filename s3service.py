3526ead5447b751f0923e29e4164f5ada264616e
Zack Climes <climeszt@mail.uc.edu>
2016-03-18 17:29:25 -0400

    Finished get and put. Put uses boto3 to put strings in the specified bucket at the specified key, get just makes a GET request to the aws URL and returns the response content.

--- s3service.py
+++ s3service.py
@@ -1,57 +1,48 @@
+import sys
 import boto3
+import requests
 from flask import Flask, jsonify
 from botocore import exceptions
 
 app = Flask(__name__, instance_relative_config=True)
-app.config.from_pyfile('aws_config.py')
+s3 = boto3.resource('s3')
 
-# app.config['SECRET_KEY']
-
-s3 = boto3.client('s3',
-                  aws_access_key_id=app.config['ACCESS_KEY'],
-                  aws_secret_access_key=app.config['SECRET_KEY'])
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+S3_URL = 'https://%s.s3.amazonaws.com/%s'
 
 
-@app.route('/s3service/get-base64-string/<string:bucket_name>/<string:key>', methods=['GET'])
-def get_base64_string(bucket_name, key):
-    print "get-base64-string"
+@app.route('/s3service/get-string/<string:bucket_name>/<string:key>', methods=['GET'])
+def get_string(bucket_name, key):
+    try:
+        content = requests.get(S3_URL % (bucket_name, key)).content
+
+        # if it's xml, that means the key wasn't found
+        if content[0] == '<':
+            return jsonify({'error': 'Key not found'})
+        else:
+            return jsonify({key: content})
+
+    except Exception as e:
+        return jsonify({'error': e})
+
+
+@app.route('/s3service/put-string/<string:bucket_name>/<string:key>/<string:value>', methods=['GET'])
+def put_string(bucket_name, key, value):
     bucket = get_bucket(bucket_name)
 
-    # if the bucket doesn't exist, return a 404 error
-    if not bucket:
-        return jsonify({'error': 404})
-
-    value = s3.Object(bucket_name, key).get().read().decode("Base64")
-    return jsonify({'bucket': bucket_name, 'key': key, 'value': value})
-
-
-@app.route('/s3service/put-base64-string/<string:bucket_name>/<string:key>/<string:value>', methods=['GET'])
-def put_base_64_string(bucket_name, key, value):
-    print "put-base64-string"
-    try:
-        bucket = get_bucket(bucket_name)
-    except Exception as e:
-        print e
-
-    # if the bucket doesn't exist, make one
-    if not bucket:
-        s3.create_bucket(Bucket=bucket_name)
+    # handle errors
+    if isinstance(bucket, dict) and 'error' in bucket:
+        # if the bucket doesn't exist, make one
+        if bucket['error'] == 'Bucket does not exist':
+            s3.create_bucket(Bucket=bucket_name)
+        else:
+            return jsonify(bucket)
 
     s3.Object(bucket_name, key).put(Body=value)
     return jsonify({'bucket': bucket_name, 'key': key, 'value': value})
 
 
-@app.route('/s3service/delete')
-def delete():
-    return ''
-
-
 def get_bucket(bucket_name):
-    bucket = s3.Bucket(bucket_name)
+    result = s3.Bucket(bucket_name)
 
     try:
         s3.meta.client.head_bucket(Bucket=bucket_name)
@@ -59,9 +50,14 @@ def get_bucket(bucket_name):
         # if it was a 404 error, the bucket doesn't exist
         error_code = int(e.response['Error']['Code'])
         if error_code == 404:
-            bucket = False
+            result = {'error': 'Bucket does not exist'}
+    except exceptions.NoCredentialsError as e:
+        result = {'error': 'No credentials found'}
+    except:
+        result = {'error': sys.exec_info()[0]}
 
-    return bucket
+    return result
+
 
 if __name__ == '__main__':
     app.run()
